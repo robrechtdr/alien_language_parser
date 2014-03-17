2a2ed9c5c5528e4a092e81e85e546bde2fcfe1b4
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2014-03-17 14:27:12 +0100

    Implement docstrings with 'googley'-ish style
    I am not going to use Sphinx docs with this so then the googley
    docstring style is preferable.
    See https://pythonhosted.org/an_example_pypi_project/sphinx.html#full-code-example

--- alien_language_parser.py
+++ alien_language_parser.py
@@ -7,21 +7,30 @@
 
 
 def is_prime(n):
-    """
-    >>> is_prime(0)
-    False
+    """Check if n is a prime.
 
-    >>> is_prime(1)
-    False
+    Args:
+        n (int): The number to check.
 
-    >>> is_prime(2)
-    True
+    Returns:
+        bool.
 
-    >>> is_prime(30)
-    False
+    Doctests:
+        >>> is_prime(0)
+        False
 
-    >>> is_prime(89)
-    True
+        >>> is_prime(1)
+        False
+
+        >>> is_prime(2)
+        True
+
+        >>> is_prime(30)
+        False
+
+        >>> is_prime(89)
+        True
+
     """
     l = range(2, n)
     for m in range(len(l)):
@@ -33,45 +42,52 @@ def is_prime(n):
         return True
 
 
-def operate(operable_string):
+def calculate(operable_group):
+    """Calculate the result of an operable group.
+
+    Args:
+        operable_group (str): The group to calculate a result of.
+
+    Returns:
+        str.
+
+    Doctests:
+        >>> calculate('1 LEFT 2')
+        '1'
+
+        >>> calculate('1 RIGHT 2')
+        '2'
+
+        >>> calculate('32 RIGHT 4')
+        '4'
+
+        >>> calculate('1 DOWN 2')
+        '1'
+
+        >>> calculate('1 DOWN 3')
+        '0'
+
+        >>> calculate('1 UP 2')
+        '1'
+
+        >>> calculate('8 UP 3')
+        '0'
+
     """
-    >>> operate('1 LEFT 2')
-    '1'
-
-    >>> operate('1 RIGHT 2')
-    '2'
-
-    >>> operate('32 RIGHT 4')
-    '4'
-
-    >>> operate('1 DOWN 2')
-    '1'
-
-    >>> operate('1 DOWN 3')
-    '0'
-
-    >>> operate('1 UP 2')
-    '1'
-
-    >>> operate('8 UP 3')
-    '0'
-
-
-    """
-    operation_blocks = operable_string.split()
+    operation_blocks = operable_group.split()
     left_operand = operation_blocks[0]
     right_operand = operation_blocks[2]
-    if "LEFT" in operable_string:
+    if "LEFT" in operable_group:
         return left_operand
-    elif "RIGHT" in operable_string:
+    elif "RIGHT" in operable_group:
         return right_operand
-    elif "UP" in operable_string:
+    elif "UP" in operable_group:
         if int(right_operand) % int(left_operand) == 0:
             return "1"
         else:
             return "0"
 
-    elif "DOWN" in operable_string:
+    elif "DOWN" in operable_group:
         sum_blocks = int(left_operand) + int(right_operand)
         if is_prime(sum_blocks):
             return "1"
@@ -80,48 +96,63 @@ def operate(operable_string):
 
     else:
         raise Exception(
-            "Operator in '{0}' is invalid".format(operable_string))
+            "Operator in '{0}' is invalid".format(operable_group))
 
 
-def break_first_operable_group(stri, break_side="right"):
+def break_in_first_operable_group(text, break_side="right"):
+    """Break alien text into two pieces in the first operable group.
+
+    Args:
+        text (str): The alien text to break into pieces.
+
+    Kwargs:
+        break_side (str): The side in the first operable group where the
+            the text should be broken. This is either "right" or "left".
+
+    Returns:
+        str.
+
+    Doctests:
+        >>> break_in_first_operable_group('3 LEFT 4', 'right')
+        ('3 LEFT ', '4')
+
+        >>> break_in_first_operable_group('23 RIGHT 3 LEFT 12', 'right')
+        ('23 RIGHT ', '3 LEFT 12')
+
+        >>> break_in_first_operable_group('3 LEFT 4', 'left')
+        ('3', ' LEFT 4')
+
+        >>> break_in_first_operable_group('23 RIGHT 3 LEFT 12', 'left')
+        ('23', ' RIGHT 3 LEFT 12')
+
+        >>> break_in_first_operable_group('23 RIGHT 3 LEFT 12', 'bla')
+        Traceback (most recent call last):
+          File "/usr/lib/python2.7/doctest.py", line 1289, in __run
+            compileflags, 1) in test.globs
+          File "<doctest __main__.break_in_first_operable_group[4]>",
+            line 1, in <module>
+            break_in_first_operable_group('23 RIGHT 3 LEFT 12', 'bla')
+          File "alien_language_parser.py", line 125,
+            in break_in_first_operable_group
+            raise Exception("'{0}' is not a valid argument".format(break_side))
+        Exception: 'bla' is not a valid argument
+
+        >>> break_in_first_operable_group('(23 RIGHT 3 LEFT 12', 'right')
+        Traceback (most recent call last):
+          File "/usr/lib/python2.7/doctest.py", line 1289, in __run
+            compileflags, 1) in test.globs
+          File "<doctest __main__.break_in_first_operable_group[5]>",
+            line 1, in <module>
+            break_in_first_operable_group('(23 RIGHT 3 LEFT 12', 'right')
+          File "alien_language_parser.py", line 144,
+            in break_in_first_operable_group
+            raise Exception("Should start with a digit")
+        Exception: Should start with a digit
+
     """
-    >>> break_first_operable_group('3 LEFT 4', 'right')
-    ('3 LEFT ', '4')
-
-    >>> break_first_operable_group('23 RIGHT 3 LEFT 12', 'right')
-    ('23 RIGHT ', '3 LEFT 12')
-
-    >>> break_first_operable_group('3 LEFT 4', 'left')
-    ('3', ' LEFT 4')
-
-    >>> break_first_operable_group('23 RIGHT 3 LEFT 12', 'left')
-    ('23', ' RIGHT 3 LEFT 12')
-
-    >>> break_first_operable_group('23 RIGHT 3 LEFT 12', 'bla')
-    Traceback (most recent call last):
-      File "/usr/lib/python2.7/doctest.py", line 1289, in __run
-        compileflags, 1) in test.globs
-      File "<doctest __main__.break_first_operable_group[4]>",
-      line 1, in <module>
-        break_first_operable_group('23 RIGHT 3 LEFT 12', 'bla')
-      File "alien_language_parser.py", line 125, in break_first_operable_group
-        raise Exception("'{0}' is not a valid argument".format(break_side))
-    Exception: 'bla' is not a valid argument
-
-    >>> break_first_operable_group('(23 RIGHT 3 LEFT 12', 'right')
-    Traceback (most recent call last):
-      File "/usr/lib/python2.7/doctest.py", line 1289, in __run
-        compileflags, 1) in test.globs
-      File "<doctest __main__.break_first_operable_group[2]>",
-        line 1, in <module>
-        break_first_operable_group("(23 RIGHT 3 LEFT 12", 'right')
-      File "policy_brain.py", line 65, in break_first_operable_group
+    if not text[0].isdigit():
         raise Exception("Should start with a digit")
-    Exception: Should start with a digit
-    """
-    if not stri[0].isdigit():
-        raise Exception("Should start with a digit")
-    split = stri.split()
+    split = text.split()
     if break_side == "right":
         head = "{0} ".format(" ".join(split[:2]))
         remainder = " ".join(split[2:])
@@ -133,43 +164,60 @@ def break_first_operable_group(stri, break_side="right"):
     return head, remainder
 
 
-def break_parenthesis(stri, direction="l2r"):
-    """
-    >>> break_parenthesis("(2 RIGHT 3)", "l2r")
-    ('(2 RIGHT 3', '')
+def break_on_parenthesis(text, direction="l2r"):
+    """Break text on first encountered parenthesis.
 
-    >>> break_parenthesis("(1 LEFT (2 RIGHT 3))", "l2r")
-    ('(1 LEFT (2 RIGHT 3', ')')
+    Args:
+        text (str): The alien text to break into pieces.
 
-    # Doc tests won't allow string line break via parenthesis alone
-    >>> break_parenthesis(\
-        "(2 UP ((1 LEFT (2 RIGHT 3)) UP 3)) DOWN 11", "l2r")
-    ('(2 UP ((1 LEFT (2 RIGHT 3', ') UP 3)) DOWN 11')
+    Kwargs:
+        direction (str): The direction in which the text is processed to break
+            it into pieces.
+            If the direction is "l2r" then the text will be broken on the
+            first encountered right parenthesis; `)`.
+            If the direction is "r2l" then the text will be broken on the
+            first encountered left parenthesis; `(`.
 
-    >>> break_parenthesis('(2 RIGHT 3', "r2l")
-    ('', '2 RIGHT 3')
+    Returns:
+        str.
 
-    >>> break_parenthesis("(1 LEFT (2 RIGHT 3", "r2l")
-    ('(1 LEFT ', '2 RIGHT 3')
+    Doctests:
+        >>> break_on_parenthesis("(2 RIGHT 3)", "l2r")
+        ('(2 RIGHT 3', '')
 
-    >>> break_parenthesis("(2 UP ((1 LEFT (2 RIGHT 3", "r2l")
-    ('(2 UP ((1 LEFT ', '2 RIGHT 3')
+        >>> break_on_parenthesis("(1 LEFT (2 RIGHT 3))", "l2r")
+        ('(1 LEFT (2 RIGHT 3', ')')
 
-    >>> break_parenthesis('(2 RIGHT 3)', "bla")
-    Traceback (most recent call last):
-      File "/usr/lib/python2.7/doctest.py", line 1289, in __run
-        compileflags, 1) in test.globs
-      File "<doctest __main__.break_parenthesis[6]>", line 1, in <module>
-        break_parenthesis('(2 RIGHT 3)', "bla")
-      File "alien_language_parser.py", line 166, in break_parenthesis
-        raise Exception("'{0}' is not a valid argument".format(direction))
-    Exception: 'bla' is not a valid argument
+        # Doc tests won't allow string line break via parenthesis alone
+        >>> break_on_parenthesis(\
+            "(2 UP ((1 LEFT (2 RIGHT 3)) UP 3)) DOWN 11", "l2r")
+        ('(2 UP ((1 LEFT (2 RIGHT 3', ') UP 3)) DOWN 11')
+
+        >>> break_on_parenthesis('(2 RIGHT 3', "r2l")
+        ('', '2 RIGHT 3')
+
+        >>> break_on_parenthesis("(1 LEFT (2 RIGHT 3", "r2l")
+        ('(1 LEFT ', '2 RIGHT 3')
+
+        >>> break_on_parenthesis("(2 UP ((1 LEFT (2 RIGHT 3", "r2l")
+        ('(2 UP ((1 LEFT ', '2 RIGHT 3')
+
+        >>> break_on_parenthesis('(2 RIGHT 3)', "bla")
+        Traceback (most recent call last):
+          File "/usr/lib/python2.7/doctest.py", line 1289, in __run
+            compileflags, 1) in test.globs
+          File "<doctest __main__.break_on_parenthesis[6]>", line 1,
+            in <module>
+            break_on_parenthesis('(2 RIGHT 3)', "bla")
+          File "alien_language_parser.py", line 166, in break_on_parenthesis
+            raise Exception("'{0}' is not a valid argument".format(direction))
+        Exception: 'bla' is not a valid argument
 
     """
     if direction == "l2r":
-        split = stri.split(")", 1)
+        split = text.split(")", 1)
     elif direction == "r2l":
-        split = stri.rsplit("(", 1)
+        split = text.rsplit("(", 1)
     else:
         raise Exception("'{0}' is not a valid argument".format(direction))
 
@@ -180,41 +228,63 @@ def break_parenthesis(stri, direction="l2r"):
 
 # Slightly less namespace-polluting than using global
 class ParseData(object):
+    """Holds text between recursive calls of alien_eval."""
     def __init__(self):
+        #: Is prepended to text of a recursive call of alien_eval stemming
+        #: from a text beginning with a left parenthesis.
+        #:
+        #: Example :
+        #:   alien_eval("(1 LEFT (2 RIGHT 1) RIGHT 4)")
+        #:     then
+        #:   self.R2L_LAST is "(1 LEFT "
         self.R2L_LAST = ""
+
+        #: Similar to the above but it is appended instead of prepended.
+        #:
+        #: For example above:
+        #:   self.L2R_LAST is " RIGHT 4"
         self.L2R_LAST = ""
+
 
 parse_data = ParseData()
 
 
-def alien_eval(alien_string):
+def alien_eval(text):
+    """Evaluate a piece of text in our alien language to a number.
+
+    Args:
+        text (str): A piece of text with alien language syntax.
+
+    Returns:
+        int.
+
+    Doctests:
+        >>> alien_eval("(2 DOWN ((1 LEFT (2 RIGHT 3)) UP 3)) DOWN 11")
+        0
+
+        >>> alien_eval("(1 LEFT 2) RIGHT 1")
+        1
+
+        # Bonus : }
+        >>> alien_eval("8 LEFT 3 LEFT 4")
+        8
+
+        >>> alien_eval("((8 UP 3 ))")
+        0
+
+        # This case follows another parsing path
+        # which was not tested with the given examples!
+        >>> alien_eval("18 RIGHT (3 LEFT 4)")
+        3
+
     """
-    >>> alien_eval("(2 DOWN ((1 LEFT (2 RIGHT 3)) UP 3)) DOWN 11")
-    0
-
-    >>> alien_eval("(1 LEFT 2) RIGHT 1")
-    1
-
-    # Bonus : }
-    >>> alien_eval("8 LEFT 3 LEFT 4")
-    8
-
-    >>> alien_eval("((8 UP 3 ))")
-    0
-
-    # This case follows another parsing path
-    # which was not tested with the given examples!
-    >>> alien_eval("18 RIGHT (3 LEFT 4)")
-    3
-
-    """
-    if alien_string[0].isdigit():
+    if text[0].isdigit():
         # Case: "12"
-        if len(alien_string.split()) == 1:
-            return int(alien_string)
+        if len(text.split()) == 1:
+            return int(text)
         # Case: "2 RIGHT 12"
-        elif len(alien_string.split()) == 3:
-            tmp_result = operate(alien_string)
+        elif len(text.split()) == 3:
+            tmp_result = calculate(text)
             current_string = "{0}{1}{2}".format(parse_data.R2L_LAST,
                                                 tmp_result,
                                                 parse_data.L2R_LAST)
@@ -222,26 +292,26 @@ def alien_eval(alien_string):
             parse_data.L2R_LAST = ""
             return alien_eval(current_string)
         else:
-            rhead, rremainder = break_first_operable_group(alien_string,
-                                                           "right")
+            rhead, rremainder = break_in_first_operable_group(text, "right")
+
             # Case: "2 RIGHT 12 LEFT 3"
             if not rremainder.startswith('('):
-                lhead, lremainder = break_first_operable_group(rremainder,
-                                                               "left")
+                lhead, lremainder = break_in_first_operable_group(rremainder,
+                                                                  "left")
                 operable_stri = "{0}{1}".format(rhead, lhead)
-                tmp_result = operate(operable_stri)
+                tmp_result = calculate(operable_stri)
                 current_string = "{0}{1}".format(tmp_result, lremainder)
                 return alien_eval(current_string)
 
             # Case: "2 RIGHT (3 RIGHT 4)"
             else:
                 tmp_result = "{0}{1}".format(rhead, alien_eval(rremainder))
-                return int(operate(tmp_result))
+                return int(calculate(tmp_result))
 
     # Case: "(2 RIGHT ((1 LEFT (5 RIGHT 4)) UP 3))"
-    elif alien_string.startswith("("):
-        l2r_first, parse_data.L2R_LAST = break_parenthesis(alien_string, "l2r")
-        parse_data.R2L_LAST, r2l_first = break_parenthesis(l2r_first, "r2l")
+    elif text.startswith("("):
+        l2r_first, parse_data.L2R_LAST = break_on_parenthesis(text, "l2r")
+        parse_data.R2L_LAST, r2l_first = break_on_parenthesis(l2r_first, "r2l")
         return alien_eval(r2l_first)
 
     else:
